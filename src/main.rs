35f3947e257ad695293618a5d5ea6ea70707a121
Dmitry Seloustev <d.seloustev@adguard.com>
2025-08-27 17:00:27 +0700

    Enhance application setup by adding SQLite support, initializing database connection, and configuring tracing. Update .gitignore to include SQLite database file.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    init_tracing();
+
+    let db_pool = init_dbpool().await.unwrap();
+}
+
+fn init_tracing() {
+    use tracing_subscriber::{EnvFilter, filter::LevelFilter, fmt, prelude::*};
+
+    let rust_log = std::env::var(EnvFilter::DEFAULT_ENV)
+        .unwrap_or_else(|_| "sqlx=info,tower_http=debug,info".into());
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .parse_lossy(rust_log),
+        )
+        .init();
+}
+
+async fn init_dbpool() -> Result<sqlx::Pool<sqlx::Sqlite>, sqlx::Error> {
+    use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
+    use std::str::FromStr;
+
+    let db_connection_str =
+        std::env::var("DATABASE_URL").unwrap_or_else(|_| "sqlite:db.sqlite".to_string());
+
+    let dbpool = SqlitePoolOptions::new()
+        .connect_with(SqliteConnectOptions::from_str(&db_connection_str)?.create_if_missing(true))
+        .await
+        .expect("can't connect to database");
+
+    sqlx::migrate!()
+    .run(&dbpool)
+    .await
+    .expect("database migration failed");
+
+    Ok(dbpool)
 }
